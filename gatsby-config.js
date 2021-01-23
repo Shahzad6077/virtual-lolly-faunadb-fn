823a7920f95e7a51148ddd162a832deaf6d08a4a
shahzad6077 <alim6077@gmail.com>
2021-01-24 01:20:45 +0500

    comment the graphql source

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,18 +30,18 @@ module.exports = {
       },
     },
     // Simple config, passing URL
-    {
-      resolve: "gatsby-source-graphql",
-      options: {
-        // Arbitrary name for the remote schema Query type
-        typeName: "LOLLY",
-        // Field under which the remote schema will be accessible. You'll use this in your Gatsby query
-        fieldName: "lolly",
-        // Url to query from
-        url:
-          "https://virtual-lolly-shahzad.netlify.app/.netlify/functions/graphql",
-      },
-    },
+    // {
+    //   resolve: "gatsby-source-graphql",
+    //   options: {
+    //     // Arbitrary name for the remote schema Query type
+    //     typeName: "LOLLY",
+    //     // Field under which the remote schema will be accessible. You'll use this in your Gatsby query
+    //     fieldName: "lolly",
+    //     // Url to query from
+    //     url:
+    //       "https://virtual-lolly-shahzad.netlify.app/.netlify/functions/graphql",
+    //   },
+    // },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
